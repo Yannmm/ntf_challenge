302e26ef30e3abfc3989b8503211439d47c93480
r.yan <yannmm@foxmail.com>
2022-01-11 22:34:36 +0800

    first blood.

---
+++ src/componenets/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Header.css";
+import punkLogo from "../assets/header/cryptopunk-logo.png";
+import searchIcon from "../assets/header/search.png";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logoContainer">
+        <img src={punkLogo} class="punkLogo" alt="" />
+      </div>
+
+      <div className="searchBar">
+        <div className="searchContainer">
+          <img src={searchIcon} alt="" />
+        </div>
+        <input
+          className="searchInput"
+          placeholder="Collection, item or user..."
+        />
+      </div>
+
+      <div className="headerItems">
+        <p>Drops</p>
+        <p>Marketplace</p>
+        <p>Create</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
